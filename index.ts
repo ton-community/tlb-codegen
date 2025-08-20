4943e5209172b1bd75a97c3459b8fb2125aa0b6f
Tim <truecarry@gmail.com>
2025-08-20 09:58:39 +0300

    chore: fix lint errors

--- index.ts
+++ index.ts
@@ -1,13 +1,5 @@
-export {
-    getTLBCodeByAST,
-    generateCodeByAST,
-    generateCodeFromData,
-} from './src/main';
-export {
-    generateCode,
-    getTLBCode,
-    generateCodeWithGenerator,
-} from './src/node'
+export { getTLBCodeByAST, generateCodeByAST, generateCodeFromData } from './src/main';
+export { generateCode, getTLBCode, generateCodeWithGenerator } from './src/node';
 export * from './src/ast';
 export { isBigInt as isBigIntForJson, isBigIntExpr as isBigIntExprForJson } from './src/generators/typescript/utils';
 export type { CodeGenerator } from './src/generators/generator';
