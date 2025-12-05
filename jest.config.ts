da81cf5fd9affa43d7a01acad1d4f42fa78901bc
Ilyar <ilyar.software@gmail.com>
2025-12-05 09:04:39 +0100

    chore: update project setup

--- jest.config.ts
+++ jest.config.ts
@@ -5,6 +5,17 @@ const config: Config = {
     testEnvironment: 'node',
     testMatch: ['**/*.spec.ts'],
     preset: 'ts-jest',
+    silent: false,
+    coverageThreshold: {
+        global: {
+            statements: 91.06,
+            branches: 82.69,
+            functions: 93.25,
+            lines: 91.64,
+        },
+    },
+    coveragePathIgnorePatterns: ['test/', 'src/index.ts'],
+    coverageReporters: ['json-summary', 'text', 'lcov'],
 };
 
 export default config;
